ecade348c35484c8b9c4b55bae094b88dcea76bf
AnandSundar <anandsundar96@gmail.com>
2017-12-21 21:57:11 -0500

    added reusability to backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Aux from  '../../hoc/ReactAux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class layout  extends Component {
+    
+    state = {
+        showSideDrawer: true
+    }
+    
+    SideDrawerClosedhandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    render(){
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.SideDrawerClosedhandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+    
+}
 
 export default layout;
