67c0f5019a0992d75aa069710f25967456492063
AnandSundar <anandsundar96@gmail.com>
2017-12-24 20:35:44 -0500

    added form validation feedback

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import classes from './Input.css';
 const input = (props) => {
-
+    const inputClasses = [classes.InputElement];
     let inputElement = null;
+
+    if(props.invalid && props.shouldValidate) {
+        inputClasses.push(classes.Invalid);
+    }
+
     switch (props.elementType) {
         case ('input'):
             inputElement = <input 
-            className={classes.InputElement} 
+            className={inputClasses.join(' ')} 
             {...props.elementConfig} 
             value={props.value} 
             onChange={props.changed}/>;
             break;
         case ('textarea'):
             inputElement = <textarea 
-            className={classes.InputElement} 
+            className={inputClasses.join(' ')} 
             {...props.elementConfig} 
             value={props.value} 
             onChange={props.changed}/>;
@@ -21,7 +26,7 @@ const input = (props) => {
         case ('select'):
             inputElement = (
                 <select 
-                    className={classes.InputElement} 
+                    className={inputClasses.join(' ')} 
                     value={props.value} 
                     onChange={props.changed}>
                     {props.elementConfig.options.map(option => (
@@ -34,7 +39,7 @@ const input = (props) => {
             break;    
         default:
             inputElement = <input 
-            className={classes.InputElement} 
+            className={inputClasses.join(' ')} 
             {...props.elementConfig} 
             value={props.value}
             onChange={props.changed}/>;
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -98,7 +98,7 @@ class ContactData extends Component {
         if(rules.required) {
             isValid = value.trim() !== '';
         }
-        
+
         return isValid;
     }
 
@@ -133,6 +133,8 @@ class ContactData extends Component {
                     elementType={formElement.config.elementType} 
                     elementConfig={formElement.config.elementConfig}
                     value={formElement.config.value}
+                    invalid={!formElement.config.valid}
+                    shouldValidate={formElement.config.validation}
                     changed={(event) => this.inputChangedHandler(event, formElement.id)}/>  
                 ))}
                 <Button clicked={this.orderHandler} btnType="Success">ORDER</Button>
